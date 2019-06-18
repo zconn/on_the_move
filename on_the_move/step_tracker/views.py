dc62e3a380408cb040c6bdfb811bc34620bfd86c
Zack Connaughton <zaconnaughton@gmail.com>
2019-06-17 21:57:18 -0700

    updated step_entry page to include input and post event and created a layout for handling the post event in the step_tracker views. Updated settings.py to US/Pacific time.

--- on_the_move/step_tracker/views.py
+++ on_the_move/step_tracker/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def step_tracker_main(request):
-    #return HttpResponse("Testing Step Tracker Page. Working!")
+
+    if request.method == "POST":
+        if request.POST.get("entry_type") == 'activity':
+            print(request.POST.get("activity"))
+            print(request.POST.get("duration"))
+        print(request.POST.get("steps"))
     return render(request, 'step_tracker/step_entry.html')
