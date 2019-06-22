d286cd5a82534b2972d3a1cb56639804311fcadf
Zack Connaughton <zaconnaughton@gmail.com>
2019-06-21 23:37:38 -0700

    Updated step tracker to include date field. Added User1 to users. Created query that shows steps tracked for today as part of the page. Created working POST event to update sqlite3 database with steps entered in page when OK is clicked.

--- on_the_move/step_tracker/models.py
+++ on_the_move/step_tracker/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 class Steps(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     entry_date = models.DateField(blank=False)
-    activitiy = models.TextField(blank=True)
-    minutes = models.IntegerField(blank=True)
+    activitiy = models.TextField(blank=True, null=True)
+    minutes = models.IntegerField(blank=True, null=True)
     steps = models.IntegerField(blank=False)
 
     class Meta:
         verbose_name_plural = "Steps"
 
     def __str__(self):
-        return self.entry_date
+        return str(self.author) + " " + str(self.entry_date)
--- on_the_move/step_tracker/views.py
+++ on_the_move/step_tracker/views.py
@@ -1,11 +1,23 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Sum
+from step_tracker.models import Steps
 
 def step_tracker_main(request):
 
     if request.method == "POST":
-        if request.POST.get("entry_type") == 'activity':
-            print(request.POST.get("activity"))
-            print(request.POST.get("duration"))
-        print(request.POST.get("steps"))
-    return render(request, 'step_tracker/step_entry.html')
+        print(type(request.POST))
+        for x in request.POST:
+            print(x)
+        # print("User1")
+        # print(request.POST.get("date"))
+        # print(request.POST.get("steps"))
+        # if request.POST.get("entry_type") == 'activity':
+        #     print(request.POST.get("activity"))
+        #     print(request.POST.get("duration"))
+        steps_entry = Steps(author=request.user, entry_date=request.POST.get("date"), steps=request.POST.get("steps"))
+        steps_entry.save()
+    steps_today = Steps.objects.filter(author=request.user, entry_date=datetime.today()).aggregate(steps=Sum("steps"))
+    context = {'steps_today':steps_today['steps']}
+    return render(request, 'step_tracker/step_entry.html', context)
